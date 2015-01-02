bc03d235dd4496b1ddab2275cdfbcf2c00338c61
JanEltner <j.eltner@gmx.de>
2015-01-03 00:33:47 +0100

    Editor Code-Cleanup
    - done some minor cleanups
    - renamed unintuitive variable name
    - unified + checked indention
    - removed out-commented code
    - fixed a typo in DestroyIfExist
    - consequent use brackets after if/else

--- js/latexeditor.js
+++ js/latexeditor.js
@@ -1,12 +1,13 @@
-
 $(document).ready(function () {
 	var files_latexeditor_js_done = false;
 	$('#content #controls').bind('DOMNodeInserted', function(event) {
-		if( $("#content #editor").length ) { // Editor exists
+		if( $("#content #editor").length ) { 
+			// Editor exists
 			// But control bar is finished only when the editor is filled
         		$("#content #editor").bind('DOMNodeInserted', function(event2){
-            			if(files_latexeditor_js_done || !$("#editor_save").length )
+            			if(files_latexeditor_js_done || !$("#editor_save").length ){
                 			return;
+						}
 				if(isLatex($('#content #controls .last a').html())) {
 					var latexbutton = '<button id="editor_compile">'+t('files_latexeditor','LaTex')+'</button><div class="separator"></div>';
 					$('#editor_save').after(latexbutton);
@@ -24,9 +25,8 @@ function isLatex(filename){
     return getFileExtension(filename)=='tex'||getFileExtension(filename)=='latex';
 }
 
-
 function AjaxCompile(ajaxpath, path,filename,pdflatex){
-    var jqxhr = $.ajax({
+    var response = $.ajax({
         type: 'POST',
         url: ajaxpath,
         data: {
@@ -53,9 +53,9 @@ function AjaxCompile(ajaxpath, path,filename,pdflatex){
         }
     }).responseText;
 
-    jqxhr=jQuery.parseJSON(jqxhr);
+    response=jQuery.parseJSON(response);
 
-    return jqxhr;
+    return response;
 }
 
 function compileFile(filename,path){
@@ -64,7 +64,7 @@ function compileFile(filename,path){
     var pdffile="";
     var data="";
 
-    DestroIfExist("dialogcompile");
+    DestroyIfExist("dialogcompile");
     var compileDlg=$('<div id="dialogcompile"  title="'+'Compiling:'+ path+filename +'"><div id="latexresult" class="" style="width:98%;height:98%;">'+t('files_latexeditor',"Choose the compiler and click on 'Compile'...")+'</div></div>').dialog({
         modal: false,
         open: function(e, ui) {
@@ -73,11 +73,9 @@ function compileFile(filename,path){
             }).parent().replaceWith('<select id="compiler" name ="compiler" ><option value="latex">LaTeX</option><option value="pdflatex">PDFLaTeX</option></select>');
         },
         buttons: {
-            'dummy': function(e){
-            },
+            'dummy': function(e){},
             Compile: function(){
-
-    		is_compiled = true;
+				is_compiled = true;
                 $('#latexresult').html("Compiling...");
                 json=AjaxCompile(ajaxpath,path, filename,$('#compiler').val());
                 if(json){
@@ -93,15 +91,12 @@ function compileFile(filename,path){
                         $('#latexresult').css({
                             color:"darkblue"
                         });
-
                     }
-
                     $('#latexresult').append(json.data.output);
-
                 }
 
             },
-	    ViewPdf: function(){
+			ViewPdf: function(){
                 var pdfviewerpath = OC.linkTo('files_pdfviewer', 'viewer.php')+'?dir='+encodeURIComponent(json.data.path).replace(/%2F/g, '/')+'&file='+encodeURIComponent(json.data.pdffile);
                 
                 frame='<iframe id="latexresultpdf"  style="width:100%;height:100%;display:block;"></iframe>';
@@ -109,12 +104,12 @@ function compileFile(filename,path){
 
                     $('#latexresultpdf').attr('src',pdfviewerpath);
                 });
-
             },
             Close: function() {
                 $( this ).dialog( "close" );
-		if ( is_compiled )
-			window.location.reload();
+				if ( is_compiled ){
+					window.location.reload();
+				}
             }
         }
     })
@@ -128,11 +123,8 @@ function compileFile(filename,path){
         position: [x, y]
     });
 
-
     $(":button:contains('ViewPdf')").button('disable');
-    //reopenEditor();
 }
-
 
 //Tries to compile The file
 function doCompile(){
@@ -140,15 +132,13 @@ function doCompile(){
 
         var filename = $('#editor').attr('data-filename');
         var dir=$('#editor').attr('data-dir')+'/';
-
         compileFile(filename, dir);
     }
 }
 
-function DestroIfExist(idname)
+function DestroyIfExist(idname)
 {
     if(document.getElementById(idname)) {
         $("#"+idname).remove();
     }
 }
-
